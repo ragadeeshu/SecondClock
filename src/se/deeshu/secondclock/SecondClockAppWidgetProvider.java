02f6eecac7d310593f0b6cc2e7b77eb75d104130
Ragnar Mellbin <ragnar_mellbin@hotmail.com>
2014-03-20 20:15:00 +0100

    Timer giving me all sorts of not working right. Might look into making alarmmanager precise instead...

--- src/se/deeshu/secondclock/SecondClockAppWidgetProvider.java
+++ src/se/deeshu/secondclock/SecondClockAppWidgetProvider.java
@@ -35,26 +35,28 @@ public class SecondClockAppWidgetProvider extends AppWidgetProvider {
 
 	@Override
 	public void onEnabled(Context context) {
+		updateClock(context);
 		super.onEnabled(context);
-		Log.d(LOG_TAG,
-				"Widget Provider enabled.  Starting timer to update widget every second");
-		if (timer == null) {
-			timer = new Timer();
-			Calendar cal = Calendar.getInstance();
-			cal.add(Calendar.SECOND, 1);
-			cal.set(Calendar.MILLISECOND, 0);
-			timer.scheduleAtFixedRate(new ClockTimerTask(context, this),
-					cal.getTime(), 1000);
-		}
+		// Log.d(LOG_TAG,
+		// "Widget Provider enabled.  Starting timer to update widget every second");
+		// if (timer == null) {
+		// timer = new Timer();
+		// Calendar cal = Calendar.getInstance();
+		// cal.add(Calendar.SECOND, 1);
+		// cal.set(Calendar.MILLISECOND, 0);
+		// timer.scheduleAtFixedRate(new ClockTimerTask(context, this),
+		// cal.getTime(), 1000);
+		// }
 
 	}
 
-	@Override
-	public void onDisabled(Context context) {
-		super.onDisabled(context);
-		Log.d(LOG_TAG, "Widget Provider disabled. Turning off timer");
-		timer.cancel();
-	}
+	//
+	// @Override
+	// public void onDisabled(Context context) {
+	// super.onDisabled(context);
+	// Log.d(LOG_TAG, "Widget Provider disabled. Turning off timer");
+	// timer.cancel();
+	// }
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
