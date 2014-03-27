0bd6c6a7bf0ee869baf52451412a32f0edea0cd9
Ragnar Mellbin <ragnar_mellbin@hotmail.com>
2014-03-27 19:49:31 +0100

    Started working on new options that make more sense

--- gen/se/deeshu/secondclock/R.java
+++ gen/se/deeshu/secondclock/R.java
@@ -8,6 +8,10 @@
 package se.deeshu.secondclock;
 
 public final class R {
+    public static final class array {
+        public static final int pref_size_entries=0x7f050000;
+        public static final int pref_size_values=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,8 +21,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int appwidget_dark_bg=0x7f020000;
@@ -28,23 +32,26 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int Corners=0x7f090001;
-        public static final int action_settings=0x7f090002;
-        public static final int widget1label=0x7f090000;
+        public static final int Corners=0x7f0a0001;
+        public static final int action_settings=0x7f0a0002;
+        public static final int widget1label=0x7f0a0000;
     }
     public static final class layout {
         public static final int clock_layout=0x7f030000;
     }
     public static final class menu {
-        public static final int control=0x7f080000;
+        public static final int control=0x7f090000;
     }
     public static final class string {
-        public static final int action_about=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int pref_really_small=0x7f060004;
-        public static final int pref_really_small_summ=0x7f060005;
-        public static final int pref_small=0x7f060002;
-        public static final int pref_small_summ=0x7f060003;
+        public static final int action_about=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int pref_really_small=0x7f070004;
+        public static final int pref_really_small_summ=0x7f070005;
+        public static final int pref_size=0x7f070006;
+        public static final int pref_size_default=0x7f070008;
+        public static final int pref_size_summ=0x7f070007;
+        public static final int pref_small=0x7f070002;
+        public static final int pref_small_summ=0x7f070003;
     }
     public static final class style {
         /** 
@@ -68,12 +75,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int styleName=0x7f070002;
+        public static final int AppTheme=0x7f080001;
+        public static final int styleName=0x7f080002;
     }
     public static final class xml {
         public static final int clock_preferences=0x7f040000;
